44cd1cac7f48a36c7554a6992d2f64cc72277929
Robinson Bravo <robinsonbravo97@gmail.com>
2025-03-11 16:55:30 -0300

    comenzando la clase 18, delegacion de rutas

--- modularizacion/comentarios/models.py
+++ modularizacion/comentarios/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Comentario(models.Model):
+    nombre = models.CharField(max_length=255, null=False)
+    score = models.IntegerField(default=3)
+    email = models.EmailField()
+    contenido = models.TextField()
+    fecha = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nombre
