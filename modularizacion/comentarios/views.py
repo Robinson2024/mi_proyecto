4643b77b9206ff305ae4855d10a21493ac119ccf
Robinson <robinsonbravo97@gmail.com>
2025-03-12 18:33:33 -0300

    Cambios desde mi notebook, comenzando clase 20, estructuras y claves foraneas

--- modularizacion/comentarios/views.py
+++ modularizacion/comentarios/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Comentario # Importamos el modelo Comentario la tabla de la base de datos
+from .models import Comentario  # Importamos el modelo Comentario la tabla de la base de datos
 
 def test(request):
     return HttpResponse('funciona correctamente')
 
 def create(request):
-    comentario = Comentario(nombre='Juan', score=5, email='juan@example.com', contenido='este es un contenido')
-    comentario.save()
-    return HttpResponse(f'Comentario guardado con ID: {comentario.id}')
+    # comentario = Comentario(nombre='Robin', score=5, email='robin@example.com', contenido='este es un contenido')
+    # comentario.save()
+    Comentario.objects.create(nombre='Robin', score=5, email='robin@example.com', contenido='este es un contenido')
+    return HttpResponse('Ruta para probar la creacion de modelos')
+
+def delete(request):
+    #comentario = Comentario.objects.get(id=19)
+    #comentario.delete()
+    Comentario.objects.filter(id=18).delete()
+    return HttpResponse('Ruta para probar la eliminacion de modelos')
