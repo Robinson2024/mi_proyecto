4d14edd20bcf7f10b241061df241648ef070da14
Robinson Bravo <robinsonbravo97@gmail.com>
2025-03-12 16:57:14 -0300

    comenzando la clase 19, borrado de datos

--- modularizacion/comentarios/views.py
+++ modularizacion/comentarios/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Comentario # Importamos el modelo Comentario la tabla de la base de datos
 
-# Create your views here.
+def test(request):
+    return HttpResponse('funciona correctamente')
+
+def create(request):
+    comentario = Comentario(nombre='Juan', score=5, email='juan@example.com', contenido='este es un contenido')
+    comentario.save()
+    return HttpResponse(f'Comentario guardado con ID: {comentario.id}')
